398fc70305fc6a98b7c11962e96ca37a21bfd3ed
antoine-duhautbois <antoine.duhautbois@sts-sio-caen.info>
2024-12-20 16:01:33 +0100

    modif competences car erreur

--- site/Pages/Compétences.php
+++ site/Pages/Compétences.php
@@ -194,11 +194,9 @@
     <?php
     require_once("../vendor/autoload.php");
     use Symfony\Component\Yaml\Yaml;
-
-    // Chargement du fichier YAML
-    $yamlFile = 'Compétences.yaml';
+    $yamlFile = '../Portfolio/site/Pages/Compétences.yaml';
     try {
-        $data = Yaml::parseFile($yamlFile);
+    $data = Yaml::parseFile($yamlFile);
         echo "<h1>" . htmlspecialchars($data["titre"]) . "</h1>\n"; // Affichage du titre de la page avec protection contre les XSS
     
         // Parcours des domaines
